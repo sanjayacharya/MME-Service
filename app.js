22e22a368f43314cefce788637abc2e60a664d93
suyash singh <suyash.siss@gmail.com>
2018-03-11 17:50:18 +0530

    cors added in code to enable local call from angular app

--- app.js
+++ app.js
@@ -6,15 +6,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var users = require('./routes/users');
 var employee = require('./routes/empolyeeRoute');
-
 var app = express();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
